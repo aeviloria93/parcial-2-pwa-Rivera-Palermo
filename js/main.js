00b33a536fc2544b809cef06cc22122898d85588
YoelPalermo <yoel.palermo@davinci.edu.ar>
2024-06-20 22:02:30 -0300

    feat: localStorage

--- js/main.js
+++ js/main.js
@@ -53,12 +53,26 @@ async function fetchEpisodes() {
     }
 }
 
+
 function displayList(episodes) {
     const list = document.createElement('ul');
     episodes.forEach(episode => {
         const item = document.createElement('li');
         item.textContent = `Episodio ${episode.id}: ${episode.name}`;
-        item.onclick = () => displayEpisodeDetails(episode);
+        item.onclick = () => {
+
+
+
+        const historial = JSON.parse(localStorage.getItem('EpisodeHistorial')) || [];
+
+        if (!historial.includes(episode.name)) {
+            historial.push(episode.name);
+            localStorage.setItem('EpisodeHistorial', JSON.stringify(historial));
+        }
+
+
+            displayEpisodeDetails(episode);
+        };
         list.appendChild(item);
     });
     EpiCont.appendChild(list);
@@ -73,7 +87,12 @@ async function displayEpisodeDetails(episode) {
 
     // Crear y mostrar la tarjeta con los detalles del episodio
     const card = document.createElement('div');
-    card.className = 'episode-card';
+    card.className = 'episode-card p-4';
+    closeButton = document.createElement('button');
+    closeButton.textContent = 'X';
+    closeButton.setAttribute('class', 'btn closeButton bg-danger text-center border border-0 float-end');
+  
+    card.appendChild(closeButton);
 
     const title = document.createElement('h2');
     title.textContent = `Episodio ${episode.id}: ${episode.name}`;
@@ -107,12 +126,6 @@ async function displayEpisodeDetails(episode) {
         }
     }
 
-  
-  closeButton = document.createElement('button');
-  closeButton.textContent = 'X';
-  closeButton.setAttribute('class', 'btn closeButton bg-danger text-center border border-0');
-
-  card.appendChild(closeButton);
 
   closeButton.addEventListener('click', () => {
       if (card) {
@@ -130,6 +143,110 @@ EpiCont.appendChild(card);
 }
 
 fetchEpisodes();
+
+
+
+
+
+const verHistorialButton = document.getElementById('verHistorial');
+verHistorialButton.addEventListener('click', mostrarHistorial);
+
+const historyDiv = document.getElementById('history');
+
+historyDiv.setAttribute('class', 'bg-warning text-dark  rounded m-auto text-center');
+
+
+
+// mostrar el historial
+function mostrarHistorial() {
+    const historial = JSON.parse(localStorage.getItem('EpisodeHistorial')) || [];
+    const ulH = document.createElement('ul');
+    ulH.setAttribute('class', 'ulH p-5 col-md-6 m-auto'); 
+
+    const  historyContainer = document.createElement('div');
+
+    historyContainer.setAttribute ('class', 'listH text-end')
+
+//historial con datos
+    function okH() {
+
+        historial.forEach(EpisodeHistorial => {
+
+            const liH = document.createElement('li');
+            liH.textContent = (EpisodeHistorial);
+            liH.setAttribute('class', 'rounded bg-secondary  m-2 p-2 text-center');
+
+            ulH.appendChild(liH);
+        });
+
+      closeButton = document.createElement('button');
+      closeButton.textContent = 'X';
+      closeButton.setAttribute('class', 'btn closeButton bg-danger border border-0 m-4');
+      closeButton.setAttribute('id', 'close');
+  
+      historyContainer.appendChild(closeButton);
+   
+
+        const total = document.createElement('p');
+        total.textContent = `Episodios vistos: ${historial.length}`;
+        total.setAttribute('class',' bg-dark p-4 rounded text-success text-center');
+
+historyContainer.appendChild(total);
+historyContainer.appendChild(ulH);
+
+       
+        closeButton.addEventListener('click', () => {
+    
+            if (historyDiv) {
+                historyDiv.innerHTML = '';
+            }
+        })
+     
+historyDiv.appendChild(historyContainer);
+
+    }
+
+
+
+    if (historial.length > 0) {
+okH(mostrarHistorial);
+
+    } else {
+        closeButton = document.createElement('button');
+
+        closeButton.textContent = 'X';
+        closeButton.setAttribute('class', 'btn closeButton bg-danger text-center border border-0 m-3');
+        closeButton.setAttribute('id', 'close');
+    
+    
+        historyContainer.appendChild(closeButton);
+ 
+       const ulH = document.createElement('ul');
+       ulH.setAttribute('class', 'list-unstyled');
+       const liH = document.createElement('li');
+        liH.innerHTML = ('No viste ningún episodio');
+        liH.setAttribute('class', ' bg-dark text-danger fs-3 text-center border border-danger p-4 rounded col-md-6 m-auto ');
+        ulH.appendChild(liH);
+
+        historyContainer.appendChild(ulH);
+
+    
+       
+        closeButton.addEventListener('click', () => {
+    
+            if (historyDiv) {
+                historyDiv.innerHTML = '';
+            }
+        })
+        historyDiv.appendChild(historyContainer);
+      
+    }
+
+
+ 
+  
+}
+
 
 let eventoInstalacion = null;
 
